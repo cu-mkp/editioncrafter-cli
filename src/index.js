7fa445b21845745a210a5a5d9c435c13b9babf45
Nick Laiacona <nick@performantsoftware.com>
2023-03-03 11:59:53 -0500

    manifest creation wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-// TODO take a simple TEI file and turn it into a EC viewable page.
+#!/usr/bin/env node
+
+const fs = require('fs')
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
+
+const testXML = "./data/FHL_007548705_ISLETA_BAPTISMS_1.xml"
+const targetDir = "./public/test_doc"
+
+function renderManifest(surfaces) {
+
+    const manifestJSON = JSON.stringify(manifest, null, '\t')
+    const iiifPath = `${targetDir}/iiif/manifest.json`
+    fs.writeFileSync(iiifPath,manifestJSON) 
+}
+
+async function run() {
+    const xml = fs.readFileSync(testXML, "utf8")
+    const testDOM = new JSDOM(xml, { contentType: "text/xml" })
+
+    const facsEl = testDOM.getElementsByTagName('facsimile')
+    const surfaceEls = facsEl.getElementsByTagName('surface')
+
+    const surfaces = []
+    for( const surfaceEl of surfaceEls ) {
+        // get the Image API endpoint
+        // get the ID
+
+    }
+
+    renderManifest(surfaces)
+}
+
+function main() {
+    run().then(() => {
+        console.log('Done!')
+    }, (err) => {
+        console.log(`${err}: ${err.stack}`)  
+    })
+}
+
+///// RUN THE SCRIPT
+main()
