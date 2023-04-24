8a3ba56b87d56ce69148e53d7e744d7788f44a61
Camden Mecklem <c@camdenmecklem.com>
2023-04-24 14:59:20 -0400

    fix paths for global use

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const jsdom = require("jsdom")
 const { JSDOM } = jsdom
 const {CETEI} = require("./CETEI")
 
+const manifest = require("./templates/manifest.json")
+const canvas = require("./templates/canvas.json")
+const annotation = require("./templates/annotation.json")
+const annotationPage = require("./templates/annotationPage.json")
+
 // Profile ID for EditionCrafter text partials
 const textPartialResourceProfileID = 'https://github.com/cu-mkp/editioncrafter-project/text-partial-resource.md'
 
@@ -13,18 +18,23 @@ function dirExists( dir ) {
       if( !fs.existsSync(dir) ) {
         throw `ERROR: ${dir} not found and unable to create it.`;
       }
-    }  
+    }
+}
+
+// For paths provided by the user
+function processUserPath(input_path) {
+    return path.resolve(process.cwd(), input_path)
 }
 
 function convertToHTML( xml ) {
     try {
         const htmlDOM = new JSDOM()
         const ceTEI = new CETEI(htmlDOM.window)
-        const xmlDOM = new JSDOM(xml, { contentType: "text/xml" })   
+        const xmlDOM = new JSDOM(xml, { contentType: "text/xml" })
         const data = ceTEI.domToHTML5(xmlDOM.window.document)
         return data.outerHTML
     } catch( err ) {
-        console.error(`ERROR ${err}: ${err.stack}`)  
+        console.error(`ERROR ${err}: ${err.stack}`)
     }
     return null
 }
@@ -51,12 +61,12 @@ function generateTextPartial( surfaceID, textEl ) {
         // TODO parse facs URI
         // TODO this will assume facs values are unique
         if ( pbSurfaceID === `#${surfaceID}` ) {
-            const nextPbEl = pbEls[i+1] 
+            const nextPbEl = pbEls[i+1]
             scrubTree( pbEl, 'prev' )
             if( nextPbEl ) {
                 scrubTree( nextPbEl, 'next' )
                 nextPbEl.parentNode.removeChild(nextPbEl)
-            } 
+            }
             return partialTextEl.outerHTML
         }
     }
@@ -102,8 +112,6 @@ function renderPartials( surfaces, teiDocPath ) {
 }
 
 function renderTextAnnotation( annotationPageID, canvasID, textURL, annoID, format) {
-    const annotationBoilerplateJSON = fs.readFileSync("./src/templates/annotation.json")
-    const annotation = JSON.parse(annotationBoilerplateJSON)
     annotation.id = `${annotationPageID}/annotation/${annoID}`
     annotation.motivation = "supplementing"
     annotation.target = canvasID
@@ -118,17 +126,15 @@ function renderTextAnnotationPage( baseURI, canvasID, surface, apIndex ) {
     const { id: surfaceID, xmls, htmls } = surface
     if( Object.keys(xmls).length == 0 && Object.keys(htmls).length == 0 ) return null
     const annotationPageID = `${canvasID}/annotationPage/${apIndex}`
-    const annotationPageBoilerplateJSON = fs.readFileSync("./src/templates/annotationPage.json")
-    const annotationPage = JSON.parse(annotationPageBoilerplateJSON)
     annotationPage.id = annotationPageID
     let i = 0
     for( const localID of Object.keys(xmls) ) {
-        const xmlURL = `${baseURI}/tei/${localID}/${surfaceID}.xml`        
+        const xmlURL = `${baseURI}/tei/${localID}/${surfaceID}.xml`
         const annotation = renderTextAnnotation( annotationPageID, canvasID, xmlURL, i++, "text/xml" )
         annotationPage.items.push(annotation)
     }
     for( const localID of Object.keys(htmls) ) {
-        const htmlURL = `${baseURI}/html/${localID}/${surfaceID}.html`        
+        const htmlURL = `${baseURI}/html/${localID}/${surfaceID}.html`
         const annotation = renderTextAnnotation( annotationPageID, canvasID, htmlURL, i++, "text/html" )
         annotationPage.items.push(annotation)
     }
@@ -136,25 +142,18 @@ function renderTextAnnotationPage( baseURI, canvasID, surface, apIndex ) {
 }
 
 function renderManifest( manifestLabel, baseURI, surfaces, teiDocPath, thumbnailWidth, thumbnailHeight) {
-    const manifestBoilerplateJSON = fs.readFileSync("./src/templates/manifest.json")
-    const canvasBoilerplateJSON = fs.readFileSync("./src/templates/canvas.json")
-    const annotationBoilerplateJSON = fs.readFileSync("./src/templates/annotation.json")
-
-    const manifest = JSON.parse(manifestBoilerplateJSON)
     manifest.id = `${baseURI}/iiif/manifest.json`
     manifest.label = { en: [manifestLabel] }
 
     for( const surface of Object.values(surfaces) ) {
         const { id, label, imageURL, width, height } = surface
 
-        const canvas = JSON.parse(canvasBoilerplateJSON)
         canvas.id = `${baseURI}/iiif/canvas/${id}`
         canvas.height = height
         canvas.width = width
         canvas.label = { "none": [ label ] }
         canvas.items[0].id = `${canvas.id}/annotationpage/0`
 
-        const annotation = JSON.parse(annotationBoilerplateJSON)
         annotation.id = `${canvas.items[0].id}/annotation/0`
         annotation.motivation = "painting"
         annotation.target = canvas.id
@@ -178,12 +177,12 @@ function renderManifest( manifestLabel, baseURI, surfaces, teiDocPath, thumbnail
         canvas.items[0].items.push(annotation)
         const annotationPage = renderTextAnnotationPage(baseURI, canvas.id, surface, 1)
         if( annotationPage ) canvas.annotations = [ annotationPage ]
-        manifest.items.push( canvas )      
+        manifest.items.push( canvas )
     }
-    
+
     const manifestJSON = JSON.stringify(manifest, null, '\t')
     const iiifPath = `${teiDocPath}/iiif/manifest.json`
-    fs.writeFileSync(iiifPath,manifestJSON) 
+    fs.writeFileSync(iiifPath,manifestJSON)
 }
 
 function parseSurfaces(doc) {
@@ -191,7 +190,7 @@ function parseSurfaces(doc) {
      const facsEl = doc.getElementsByTagName('facsimile')[0]
      const textEls = doc.getElementsByTagName('text')
      const surfaceEls = facsEl.getElementsByTagName('surface')
-     
+
      // parse invididual surfaces and partials
      const surfaces = {}
      for( const surfaceEl of surfaceEls ) {
@@ -229,7 +228,7 @@ function renderResources( doc, htmlDoc, teiDocPath ) {
         dirExists(resourceDir)
         fs.writeFileSync(`${resourceDir}/index.html`,resourceHTMLEl.outerHTML)
     }
-} 
+}
 
 function validateTEIDoc(doc) {
     // TODO needs to have exactly 1 facs. needs to be a valid xml doc
@@ -251,7 +250,7 @@ function renderTEIDocument(options) {
     dirExists(`${teiDocPath}/iiif`)
     dirExists(`${teiDocPath}/html`)
 
-    // render complete TEI  
+    // render complete TEI
     fs.writeFileSync(`${teiDocPath}/tei/index.xml`,xml)
 
     // render complete HTML
@@ -263,11 +262,11 @@ function renderTEIDocument(options) {
     // render resources
     renderResources( doc, htmlDoc, teiDocPath )
 
-    // render manifest and partials 
+    // render manifest and partials
     const surfaces = parseSurfaces(doc)
     const documentURL = `${baseURL}/${teiDocumentID}`
     renderManifest( teiDocumentID, documentURL, surfaces, teiDocPath, thumbnailWidth, thumbnailHeight )
-    renderPartials( surfaces, teiDocPath )   
+    renderPartials( surfaces, teiDocPath )
 }
 
 async function run(options) {
@@ -293,8 +292,8 @@ function processArguments() {
     const mode = args[2]
 
     if( mode === 'process' ) {
-        const targetPath = args[3]
-        const outputPath = args[4] ? args[4] : './public'
+        const targetPath = processUserPath(args[3])
+        const outputPath = args[4] ? processUserPath(args[4]) : processRelativePath('./public')
         const baseURL = args[5] ? args[5] : 'http://localhost:8080'
         const teiDocumentID = getResourceIDFromPath(targetPath)
         const thumbnailWidth = 124
@@ -320,9 +319,9 @@ function editionCrafterCLI() {
         run(options).then(() => {
             console.log('Edition Crafter finished.')
         }, (err) => {
-            console.log(`${err}: ${err.stack}`)  
-        })    
-    }    
+            console.log(`${err}: ${err.stack}`)
+        })
+    }
 }
 
 module.exports.editionCrafterCLI = editionCrafterCLI;
