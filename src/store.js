20db17a8ce3a6ad0decafc77810236a331268a99
Nick Laiacona <nick@performantsoftware.com>
2023-05-12 15:11:49 -0400

    processing tei wip

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 const { initArchivEngine } = require("./archivengine")
 const { getResources } = require("./archivengine/resource")
 const fs = require('fs')
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
 
 const { renderTEIDocument } = require("./render")
 
@@ -28,26 +30,41 @@ async function loadTEIDocument(teiDocumentID, documentStore) {
         // get the resources, process them into a teiDocument object, store it, and return it
         const { archivEngineURL, authToken, projectID } = documentStore
         const { resourceID } = teiDocument
-        const { parentEntry, resourceEntries } = await getResources(archivEngineURL, authToken, projectID, resourceID)
-        teiDocument = processResources( parentEntry, resourceEntries )
+        const { resourceEntries } = await getResources(archivEngineURL, authToken, projectID, resourceID)
+        const renderOptions = { teiDocumentID, baseURL, teiDocumentID, thumbnailWidth, thumbnailHeight } = documentStore
+        teiDocument = processResources( resourceEntries, renderOptions )
         teiDocuments[teiDocumentID] = teiDocument
         return teiDocument
     }
 }
 
-function processResources( parentEntry, resourceEntries ) {
+function processResources( resourceEntries, renderOptions ) {
     // first, take the entries and combine them into a single xml
     // then process that xml into the teidocument obj, retaining the resourceID and loaded set to true
     // id, name, localID, parentResource, type, content
-    for( const resourceEntry of resourceEntries ) {
-        const { resourceType } = resourceEntry
-        if( resourceType == 'header' ) {
-            
-        } else {
+    const documentParts = { header: 'teiHeader', text:'text', sourceDoc: 'sourceDoc', facs:'facsimile', standOff: 'standOff' }
 
-        }
+    let teiHeaderEl;
+    const childResourceEls = []
+    for( const resourceEntry of resourceEntries ) {
+        const { resourceType, content } = resourceEntry
+        const doc = new JSDOM(content, { contentType: "text/xml" }).window.document
+        const partName = documentParts[resourceType]
+        const els = doc.getElementsByTagName(partName)
+
+        if( resourceType === 'header' ) {
+            teiHeaderEl = els[0]
+        } else {
+            for( const el of els ) {
+                childResourceEls.push(el)
+            }
+        }   
     }
 
+    const childResourcesXML = childResourceEls.map( (childResourceEl) => childResourceEl.outerHTML ).join('\n')
+    const xml = `<TEI xmlns="http://www.tei-c.org/ns/1.0">${teiHeaderEl.outerHTML}${childResourcesXML}</TEI>`
+    const teiDoc = renderTEIDocument(xml, renderOptions)
+    return teiDoc
 }
 
 function processIDMap(idMap) {
