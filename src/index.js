3ad6348dfa0b560b8c8b0e00f3c9be40958a54cc
Nick Laiacona <nick@performantsoftware.com>
2023-06-15 09:46:29 -0400

    improve cli

--- src/index.js
+++ src/index.js
@@ -53,29 +53,52 @@ function processArguments() {
 
     if( mode === 'process' ) {
         if( args.length < 4 ) return optForHelp
-        const targetPath = processUserPath(args[3])
-        const outputPath = args[4] ? processUserPath(args[4]) : processRelativePath('./public')
-        const baseURL = args[5] ? args[5] : 'http://localhost:8080'
-        const teiDocumentID = getResourceIDFromPath(targetPath)
-        const thumbnailWidth = 124
-        const thumbnailHeight = 192
-        return { mode, targetPath, outputPath, baseURL, teiDocumentID, thumbnailWidth, thumbnailHeight }
-    } else if( mode === 'server' ) {
+
+        // default settings
+        let config = {
+            mode: mode,
+            outputPath: processRelativePath('./public'),
+            baseURL: 'http://localhost:8080',
+            thumbnailWidth: 124,
+            thumbnailHeight: 192
+        }
+        let argumentOffset = 0
+        // load from config file if supplied
         if( args[3] === '-c' && args[4] ) {
             const configPath = processUserPath(args[4])
-            const config = JSON.parse( fs.readFileSync(configPath) )
-            config.teiDocuments = {
-                'fr640_3r-3v-example': processUserPath('./data/fr640_3r-3v-example.xml')
+            argumentOffset = 2 
+            try {
+                fs.readFileSync(configPath)
+                config = { ...config, ...JSON.parse( fs.readFileSync(configPath) ) } 
+            } catch(e) {
+                console.log(`Unable to parse config file: ${configPath}.`)
             }
-            return { mode, ...config }
-        } 
+        }
+
+        // parse command line params
+        if( args[3+argumentOffset] ) config.targetPath = processUserPath(args[3+argumentOffset])
+        if( args[4+argumentOffset]  ) config.outputPath = processUserPath(args[4+argumentOffset])
+        if( args[5+argumentOffset] ) config.baseURL = args[5+argumentOffset] 
+        config.teiDocumentID = getResourceIDFromPath(config.targetPath)
+        return config
+    } else if( mode === 'server' ) {
+        console.log('Server mode is still under development.')
+        return optForHelp
+        // if( args[3] === '-c' && args[4] ) {
+        //     const configPath = processUserPath(args[4])
+        //     const config = JSON.parse( fs.readFileSync(configPath) )
+        //     config.teiDocuments = {
+        //         'fr640_3r-3v-example': processUserPath('./data/fr640_3r-3v-example.xml')
+        //     }
+        //     return { mode, ...config }
+        // } 
     }
 
     return optForHelp
 }
 
 function displayHelp() {
-    console.log(`Usage: editioncrafter <command> [-c config_path]|[<tei_path> <output_path>]` );
+    console.log(`Usage: editioncrafter <command> [-c config_path]|[<tei_path> <output_path> <base_url>]` );
     console.log("Edition Crafter responds to the following <command>s:")
     console.log("\server: Run as a server, requires config_path.")
     console.log("\process: Process the TEI Document into a manifest, partials, and annotations.")
