8273c8f68bd849ece558628d0cfc817717cdf1e6
Camden Mecklem <c@camdenmecklem.com>
2024-11-27 15:54:11 -0500

    get everything working except tagged elements

--- src/db/index.js
+++ src/db/index.js
@@ -21,14 +21,15 @@ function populateTables(db) {
     );
     CREATE TABLE surfaces (
       id INTEGER PRIMARY KEY,
+      xml_id STRING,
       name STRING,
       position INTEGER,
       document_id INTEGER REFERENCES documents(id)
     );
     CREATE TABLE layers (
       id INTEGER PRIMARY KEY,
-      name STRING,
-      surface_id INTEGER REFERENCES surfaces(id)
+      xml_id STRING,
+      document_id INTEGER REFERENCES documents(id)
     );
     CREATE TABLE taxonomies (
       id INTEGER PRIMARY KEY,
@@ -48,7 +49,8 @@ function populateTables(db) {
       xml_id STRING,
       type STRING,
       category_id INTEGER REFERENCES categories(id),
-      layer_id INTEGER REFERENCES layers(id)
+      layer_id INTEGER REFERENCES layers(id),
+      surface_id INTEGER REFERENCES surfaces(id)
     );`,
   )
 }
@@ -77,6 +79,11 @@ async function parseXml(db, path) {
 
   const taxonomies = xml.querySelectorAll('taxonomy')
 
+  const documentId = parseDocument(db, xml)
+
+  parseSurfaces(db, xml, documentId)
+  parseLayers(db, xml, documentId)
+
   for (const tax of taxonomies) {
     const xmlId = tax.getAttribute('xml:id')
     const biblEl = tax.querySelector('bibl')
@@ -96,6 +103,24 @@ async function parseXml(db, path) {
   }
 }
 
+function parseDocument(db, xml) {
+  const titleEl = xml.querySelector('teiHeader > fileDesc > titleStmt > title')
+  const name = titleEl?.textContent
+    ? titleEl.textContent.trim()
+    : undefined
+
+  if (!name) {
+    console.error('Document has no title. Please add one.')
+    process.exit(1)
+  }
+
+  const { lastInsertRowid } = db
+    .prepare('INSERT INTO documents (name) VALUES (?)')
+    .run(name)
+
+  return lastInsertRowid
+}
+
 function parseCategories(db, el, taxonomyId, parentCatId) {
   const categories = el.querySelectorAll(':scope > category')
 
@@ -104,7 +129,7 @@ function parseCategories(db, el, taxonomyId, parentCatId) {
     const desc = cat.querySelector('catDesc')
 
     if (!desc) {
-      console.error(`${xmlId} does not have a name (a <catDesc> element) and will be skipped.`)
+      console.error(`Category ${xmlId} does not have a name (a <catDesc> element) and will be skipped.`)
       continue
     }
 
@@ -132,6 +157,46 @@ function parseCategories(db, el, taxonomyId, parentCatId) {
     if (childCategories.length > 0) {
       parseCategories(db, cat, taxonomyId, categoryId)
     }
+  }
+}
+
+function parseLayers(db, doc) {
+  const layers = doc.querySelectorAll('text, sourceDoc')
+
+  for (const layer of layers) {
+    const xmlId = layer.getAttribute('xml:id')
+
+    const { lastInsertRowid } = db
+      .prepare('INSERT INTO layers (xml_id) VALUES (?)')
+      .run(xmlId)
+
+    parseTaggedElements(db, layer, lastInsertRowid)
+  }
+}
+
+function parseTaggedElements(db, layerEl, layerId) {
+  const taggedElements = layerEl.querySelectorAll('[ana]')
+}
+
+function parseSurfaces(db, xml, documentId) {
+  const surfaces = xml.querySelectorAll('surface')
+
+  for (let i = 0; i < surfaces.length; i++) {
+    const surface = surfaces[i]
+    const xmlId = surface.getAttribute('xml:id')
+
+    const labelEl = surface.querySelector('label')
+
+    if (!labelEl) {
+      console.error(`Surface ${xmlId} does not have a name (a <label> element) and will be skipped.`)
+      continue
+    }
+
+    const name = labelEl.textContent
+
+    db
+      .prepare('INSERT INTO surfaces (name, xml_id, document_id, position) VALUES (?, ?, ?, ?)')
+      .run(name, xmlId, documentId, i)
   }
 }
 
